559b15037171d05e415c4a729675a999fd344680
yongxincao <yongxin.cao@cienet.com.cn>
2023-11-29 17:12:24 +0800

    init commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let search_string = &args[1];
+    let file_path = &args[2];
+
+    println!("search string:{search_string}");
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
+}
